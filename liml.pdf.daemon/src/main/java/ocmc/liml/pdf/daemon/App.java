f2ed959e57ad8a289e595cda0931fbec4808788a
Michael Colburn <mac002@theColburns.us>
2018-01-31 07:19:51 -0500

    Initial

---
+++ liml.pdf.daemon/src/main/java/ocmc/liml/pdf/daemon/App.java
@@ -0,0 +1,13 @@
+package ocmc.liml.pdf.daemon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
